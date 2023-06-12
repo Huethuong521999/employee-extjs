59d228691ecd7cf731548fe07bc1c10cd5fe3d2b
nhiempc <nhiem10596@gmail.com>
2023-06-12 21:25:28 +0700

    require check more info

--- classic/src/view/user/TabMoreInfo.js
+++ classic/src/view/user/TabMoreInfo.js
@@ -14,22 +14,32 @@ Ext.define('Admin.view.user.TabMoreInfo', {
             },
             items: [
                 {
-                    fieldLabel: 'Họ và tên',
+                    fieldLabel: 'Tên công ty',
                     allowBlank: false,
-                    name: 'name',
-                    cls: 'inputField'
+                    name: 'company',
+                    cls: 'inputField',
+                    responsiveConfig: {
+                        'width >= 768': {
+                            width: '50%'
+                        },
+                        'width < 768 ': {
+                            width: '100%'
+                        }
+                    }
                 },
                 {
-                    fieldLabel: 'Tên tài khoản',
+                    fieldLabel: 'Địa chỉ',
                     allowBlank: false,
-                    name: 'username',
-                    cls: 'inputField'
-                },
-                {
-                    fieldLabel: 'Email',
-                    allowBlank: false,
-                    name: 'email',
-                    cls: 'inputField'
+                    name: 'address',
+                    cls: 'inputField',
+                    responsiveConfig: {
+                        'width >= 768': {
+                            width: '50%'
+                        },
+                        'width < 768 ': {
+                            width: '100%'
+                        }
+                    }
                 }
             ]
         }
